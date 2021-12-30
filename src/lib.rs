2221d44b6b6919fa46a7ed9d4cbea9931364c4f8
John Baber-Lucero <git@frundle.com>
2021-12-30 18:01:43 +0000

    Start using tui library

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,31 @@ extern crate edhex_core as ec;
 
 use std::num::NonZeroUsize;
 use std::io::Read;
-use cursive::views::{
-    LinearLayout,
-    TextView,
-    ResizedView
+use crossterm::{
+    event::{self, Event as CEvent, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use std::fs;
+use std::io;
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, Instant};
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{
+        Block, BorderType, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Tabs,
+    },
+    Terminal,
 };
 
+enum Event<I> {
+    Input(I),
+    Tick,
+}
 
-// pub fn byte_numbers(state:&ec::State) -> String {
-//     return "".to_owned();
-// }
 
 pub fn cargo_version() -> Result<String, String> {
     if let Some(version) = option_env!("CARGO_PKG_VERSION") {
@@ -25,7 +40,7 @@ pub fn cargo_version() -> Result<String, String> {
     return Err("Version unknown (not compiled with cargo)".to_string());
 }
 
-pub fn actual_runtime(filename:&str) -> i32 {
+pub fn actual_runtime(filename:&str, color: bool) -> i32 {
     let file = match ec::filehandle(filename) {
         Ok(Some(filehandle)) => {
             Some(filehandle)
@@ -84,18 +99,13 @@ pub fn actual_runtime(filename:&str) -> i32 {
         last_search: None,
     };
 
-    let mut siv = cursive::default();
-    siv.add_global_callback('q', |s| s.quit());
-
-    let explanation = TextView::new("hjkl or ←↓↑→ to navigate | q to quit").align(cursive::align::Align::top_left());
+    let _explanation = "hjkl or ←↓↑→ to navigate | q to quit";
 
     /* Addresses for left column starting at 0 */
     let addresses = state.addresses(0);
     let addresses_s = addresses.iter().map(|x|
             ec::address_display(*x, state.radix, "", false))
             .collect::<Vec<String>>().join("\n");
-    let line_numbers = TextView::new(addresses_s)
-            .align(cursive::align::Align::top_left());
 
     /* Rows of bytes for middle column */
     let mut middle_column_string = "".to_owned();
@@ -118,18 +128,123 @@ pub fn actual_runtime(filename:&str) -> i32 {
         last_column_string   += "\n";
     }
 
+    /* Beginning of tui-crossterm boilerplate */
+    enable_raw_mode().expect("Cannot run in raw mode");
 
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
 
-    let bytes_display = TextView::new(format!("{}", middle_column_string)).align(cursive::align::Align::top_left());
-    let chars_display = TextView::new(format!("{}", last_column_string)).align(cursive::align::Align::top_left());
+    /* input loop */
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
 
-    let main_layout = LinearLayout::horizontal().child(line_numbers).child(bytes_display).child(chars_display);
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events"
+) {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
 
-    let total_layout = ResizedView::with_full_width(ResizedView::with_full_height(LinearLayout::vertical().child(explanation).child(main_layout)));
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
 
-    siv.add_layer(total_layout);
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend);
+    if terminal.is_err() {
+        println!("Could not render terminal");
+        return 1;
+    }
+    let mut terminal = terminal.unwrap();
+    if terminal.clear().is_err() {
+        println!("Could not clear terminal");
+        return 0;
+    }
 
-    siv.run();
+    /* End of tui-crossterm boilerplate */
+
+    /* render loop */
+    loop {
+        terminal.draw(|rect| {
+            let size = rect.size();
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(6),
+                        Constraint::Min(2),
+                        Constraint::Length(16),
+                    ]
+                    .as_ref(),
+                )
+                .split(size);
+
+            let chars = Paragraph::new("pet-CLI 2020 - all rights reserved")
+                .style(Style::default().fg(Color::LightCyan))
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::White))
+                        .border_type(BorderType::Plain),
+                );
+
+            let addresses = Paragraph::new("Addresses")
+                .style(Style::default().fg(Color::LightCyan))
+                .alignment(Alignment::Left)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::White))
+                        .border_type(BorderType::Plain),
+                );
+
+            let bytes = Paragraph::new("Bytes")
+                .style(Style::default().fg(Color::LightCyan))
+                .alignment(Alignment::Left)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::White))
+                        .border_type(BorderType::Plain),
+                );
+
+            rect.render_widget(addresses, chunks[0]);
+            rect.render_widget(bytes, chunks[1]);
+            rect.render_widget(chars, chunks[2]);
+        });
+
+        let received = rx.recv();
+        if received.is_err() {
+            println!("Failed to parse input");
+            return 3;
+        }
+        let received = received.unwrap();
+        match received {
+            Event::Input(event) => match event.code {
+                KeyCode::Char('q') => {
+                    if disable_raw_mode().is_err() || terminal.show_cursor().is_err() {
+                        println!("Couldn't clean up terminal before quitting.");
+                    }
+                    break;
+                }
+                KeyCode::Char('h') => {},
+                KeyCode::Char('p') => {},
+                _ => {}
+            },
+            Event::Tick => {}
+        }
+    }
 
     0
 }
